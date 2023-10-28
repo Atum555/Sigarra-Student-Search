f379934f38e9cd0dc268125c86c7daa045795d5f
Cláudio <claudiomeireles555@gmail.com>
2023-10-28 18:58:40 +0100

    Refactor

--- main.py
+++ main.py
@@ -3,30 +3,31 @@ import src.sigarra as sigarra
 from src.bcolors import bcolors
 
 
-def main():
-    global YEARS_TO_SEARCH, COURSE_TO_SEARCH, MAX_THREADS
-    student_Data = sigarra.get_students_in(COURSE_TO_SEARCH, (SI_SESSION, SI_SECURITY), YEARS_TO_SEARCH, MAX_THREADS)
-    
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    data_dir = os.path.join(script_dir, "data")
-    if not os.path.exists(data_dir):
-        os.makedirs(data_dir)
-    with open(os.path.join(data_dir, f"students_{COURSE_TO_SEARCH}.txt"), 'w') as file:
-        for student in student_Data: file.write(f"{student[0]}\t{student[1]}\t{student[2]}\t{student[3]}\n")
-    print(f"Saved to {bcolors.WARNING}{os.path.join(data_dir, f'students_{COURSE_TO_SEARCH}.txt')}{bcolors.ENDC}")
+def main_sigarra() -> None:
+    # Invalid arguments or help
+    if len(sys.argv) < 3 or sys.argv[2] in ["help", "-h"]:
+        to_print =  f"{bcolors.OKBLUE}Usage:{bcolors.ENDC} {sys.argv[0]} sigarra "
+        to_print += f"<{bcolors.OKGREEN}cookies_file{bcolors.ENDC}> [-y<years_to_search>] [-c<course_to_search>] [-t<max_threads>]"
+        print(to_print)
+        to_print =  f"{bcolors.OKBLUE}Example/Default:{bcolors.ENDC} {sys.argv[0]} sigarra "
+        to_print += f"{bcolors.UNDERLINE}{bcolors.WARNING}cookies.txt{bcolors.ENDC} -y123 -c22841 -t50"
+        print(to_print)
+        to_print =  f"{bcolors.OKBLUE}Cookies file:{bcolors.ENDC} must be a text file with {bcolors.FAIL}SI_SESSION{bcolors.ENDC} on the first line and {bcolors.FAIL}SI_SECURITY{bcolors.ENDC} on the second line! "
+        print(to_print)
+        exit(1)
 
-
-if __name__ == "__main__":
     # Define default values
-    with open(sys.argv[1], 'r') as file:
-        SI_SESSION  = file.readline().strip()
-        SI_SECURITY = file.readline().strip()
+    try:
+        with open(sys.argv[2], 'r') as file:
+            SI_SESSION  = file.readline().strip()
+            SI_SECURITY = file.readline().strip()
+    except: print(f"{bcolors.FAIL}ERROR{bcolors.ENDC} with cookies file!"); exit(1)
     YEARS_TO_SEARCH = ("1","2","3")
     COURSE_TO_SEARCH = "22841"
     MAX_THREADS = 50
 
     # Parse arguments
-    for x in sys.argv[2:]:
+    for x in sys.argv[3:]:
         match x[:2]:
             case "-y":
                 YEARS_TO_SEARCH = tuple(y for y in x[2:])
@@ -35,10 +36,35 @@ if __name__ == "__main__":
             case "-t":
                 MAX_THREADS = int(x[2:])
             case _:
-                print("Invalid argument: {x}")
-                exit()
+                print(f"{bcolors.FAIL}Invalid argument:{bcolors.ENDC} {bcolors.UNDERLINE}{x}{bcolors.ENDC}")
+                exit(1)
+    
+    # Get Student Data
+    studentData = sigarra.get_students_in(COURSE_TO_SEARCH, (SI_SESSION, SI_SECURITY), YEARS_TO_SEARCH, MAX_THREADS)
+    
+    # Save Data to a folder
+    scriptDir = os.path.dirname(os.path.abspath(__file__))
+    dataFolderDir = os.path.join(scriptDir, "data")
+    if not os.path.exists(dataFolderDir): os.makedirs(dataFolderDir)
+    dataDir = os.path.join(dataFolderDir, f"students_{COURSE_TO_SEARCH}_{''.join(sorted(YEARS_TO_SEARCH))}.txt")
+    with open(dataDir, 'w') as file:
+        for student in studentData: file.write("\t".join(x for x in student))
+    print(f"Saved to {bcolors.WARNING}{dataDir}{bcolors.ENDC}")
+    return None
 
-    #print(filter_student_IDs((202300600,)))
-    #exit()
 
-    main()
+def main_phone():
+    pass
+
+if __name__ == "__main__":
+    # Invalid number of arguments or help
+    def print_help() -> None:
+        print(f"{bcolors.OKCYAN}Usage:{bcolors.ENDC} {sys.argv[0]} [ sigarra | phone ]")
+        print(f"{bcolors.OKCYAN}For more help write:{bcolors.ENDC} {sys.argv[0]} [ sigarra | phone ] {bcolors.FAIL}-h{bcolors.ENDC}")
+        exit(1)
+    if len(sys.argv) < 2 or sys.argv[1] in ["help", "-h"]: print_help()
+
+    match sys.argv[1]:
+        case "sigarra": main_sigarra()
+        case "phone": main_phone()
+        case _: print_help()
--- src/phoneNumber.py
+++ src/phoneNumber.py
@@ -1,23 +1,24 @@
-import csv
-import sys
+def getMatches(data:tuple, csvFile:str) -> tuple:
+    def getCsvData(filename:str) -> tuple:
+        """
+        Reads a CSV file and returns the email, name and phone number.
 
-def getcsvdata(filename:str):
-    data = []
-    with open(filename, 'r') as csv_file:
-        csv_reader = csv.reader(csv_file)
-        for line in csv_reader:
-            data += [[line[3].strip(), line[4].strip(), line[6].strip()]]
-        data.pop(0)
-    for i in range(len(data)):
-        data[i][2] = data[i][2].replace(" ", "").replace("+351", "")
+        Args:
+            filename (str): The path to the CSV file.
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <filename>")
-        sys.exit(1)
-    getcsvdata(sys.argv[1])       
+        Returns:
+            tuple: A tuple of data represented as a tuple of three strings:
+                (email, name, phone_number).
+        """
+        import csv
+        data = []
+        with open(filename, 'r', encoding="cp1252") as csvFile:
+            csvReader = csv.reader(csvFile)
+            for line in csvReader:
+                data += [[line[3].strip(), line[4].strip(), line[6].strip()]]
+            data.pop(0)
+        for i in range(len(data)):
+            data[i][2] = data[i][2].replace(" ", "").replace("+351", "")
+        return tuple(tuple(sub) for sub in data)
 
-
-if __name__ == "__main__":
-    main()
-    
+    return (0,)
